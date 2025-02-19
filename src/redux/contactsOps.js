743f82b6c8063ee7bfb3ddcce676811e39694f6c
EugeneyCh <eugeney@ua.fm>
2025-02-19 20:02:47 +0200

    IV-Day

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -5,7 +5,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 axios.defaults.baseURL = "https://64d87f405f9bf5b879ce4838.mockapi.io";
 
 export const fetchContacts = createAsyncThunk(
-  "contacts/fetchContacts",
+  "contacts/fetchAll",
   async ({ signal }, thunkAPI) => {
     try {
       const { data } = await axios.get("/contacts", { signal });
@@ -15,23 +15,25 @@ export const fetchContacts = createAsyncThunk(
     }
   }
 );
-export const deleteContact = createAsyncThunk(
-  "contacts/deleteContact",
-  async (id, thunkAPI) => {
-    try {
-      await axios.delete(`/contacts/${id}`);
-      return id;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
+
 export const addContact = createAsyncThunk(
   "contacts/addContact",
   async (body, thunkAPI) => {
     try {
       const { data } = await axios.post("/contacts", body);
       return data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (id, thunkAPI) => {
+    try {
+      await axios.delete(`/contacts/${id}`);
+      return id;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
     }
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,45 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 import { addContact, deleteContact, fetchContacts } from "./contactsOps";
 
 const initialState = {
-  items: [
-    // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ],
-  isLoading: false,
-  isError: null,
+  items: [],
+  loading: false,
+  error: null,
 };
 
 const contactsSlice = createSlice({
   name: "contacts",
   initialState,
-  // reducers: {
-  //   fetchInProgress(state) {
-  //     state.isLoading = true;
-  //   },
-  //   fetchSuccess(state, action) {
-  //     state.isLoading = false;
-  //     state.isError = null;
-  //     state.items = action.payload;
-  //   },
-  //   fetchError(state, action) {
-  //     state.isLoading = false;
-  //     state.isError = action.payload;
-  //   },
-  // },
   extraReducers: (builder) => {
     builder
       .addCase(fetchContacts.fulfilled, (state, action) => {
         state.items = action.payload;
-        state.isLoading = false;
+        state.loading = false;
       })
       .addCase(fetchContacts.pending, (state) => {
-        state.isLoading = true;
+        state.loading = true;
       })
       .addCase(fetchContacts.rejected, (state, action) => {
-        state.isLoading = false;
-        state.isError = action.payload;
+        state.loading = false;
+        state.error = action.payload;
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.items = state.items.filter((item) => item.id != action.payload);
